8d8438721cd3b8491cea69378a9d5a570e32c43f
Bablu kumar <37810268+kumarbablu5598@users.noreply.github.com>
2024-01-22 20:39:33 +0530

    program 1

--- ControlFlow.java
+++ ControlFlow.java
@@ -6,19 +6,18 @@ import java.util.*;
 class Result {
 
 
-  public static String[] calculateGrade(int[][] students_marks) 
-	{
+    public String[] calculateGrade(int[][] students_marks) {
         int m = students_marks.length;
-		    int n = students_marks[0].length;
-        String[] Ans = new String[n];
+        int n = students_marks[0].length;
+        String[] Ans = new String[m];
         for(int i=0;i<m;i++)
-		    {
+        {
             float avg = 0;
-            for (int j=0;j<n;j++){
-			     {
-			      avg += students_marks[i][j];
-			     }
-			
+            for (int j=0;j<n;j++)
+            {
+               avg += students_marks[i][j];
+            }
+            
                
             avg = avg/n;
             if(avg >= 90) Ans[i] = "A+";
@@ -29,34 +28,37 @@ class Result {
             else Ans[i] = "F";
         }
         return Ans;
+
     }
+
 }
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args)   {
         Scanner sc = new Scanner(System.in);
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+         
 
-        int students_marksRows = Integer.parseInt(sc.next().trim());
+        int students_marksRows = sc.nextInt();
+        int students_marksColumns = sc.nextInt();
+		
+		
 		int[][] students_marks = new int[students_marksRows][students_marksColumns];
 		for(int i = 0; i < students_marksRows; i++)
         {
-			   for(int j = 0; j < students_marksColumns; j++)
-			   {
-				  students_marks[i][j] = Integer.parseInt(sc.next().trim());				
-			   }
-      }
+			for(int j = 0; j < students_marksColumns; j++)
+			{
+				students_marks[i][j] = sc.nextInt();				
+			}
+        }
+        Result  obj = new Result();
 
-        String[] result = Result.calculateGrade(students_marks);
+        String[] result = obj.calculateGrade(students_marks);
 		
 		for(int i = 0; i < result.length; i++)
         {
 			System.out.println(result[i]);
-            bufferedWriter.write(result[i]+"\n");
+            
         }       
-        bufferedWriter.close();
+        
     }
-	
 }
-
-
